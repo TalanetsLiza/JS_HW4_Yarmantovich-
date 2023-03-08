c9fac04ce9c8e1533622f86053e6e7f4bbdf9ddd
TalanetsLiza <yarmantovich1999@mail.ru>
2023-03-08 20:49:42 +0300

    add: complete task 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,3 +9,17 @@
         console.log(arr[i]);
     }
 }
+
+{
+    // Task 2
+
+    const arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+    
+    for ( let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (item > -10 && item < -3) {
+            console.log(item);
+        }
+    }
+
+}
